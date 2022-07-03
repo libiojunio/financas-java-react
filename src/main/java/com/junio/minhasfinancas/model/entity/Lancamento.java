611efb374ad2d5e6a0f4e83ee5bf7dc0517b7e25
Libiojunio <libiojss@gmail.com>
2022-07-03 16:18:07 -0300

    Atualizando entidades para usar lombok.

--- src/main/java/com/junio/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/junio/minhasfinancas/model/entity/Lancamento.java
@@ -1,5 +1,7 @@
 package com.junio.minhasfinancas.model.entity;
 
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.*;
@@ -9,6 +11,11 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "lancamento", schema = "financas")
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
 public class Lancamento {
 
   @Id
@@ -41,94 +48,16 @@ public class Lancamento {
   @Enumerated(EnumType.STRING)
   private StatusLancamento statusLancamento;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public Integer getMes() {
-    return mes;
-  }
-
-  public void setMes(Integer mes) {
-    this.mes = mes;
-  }
-
-  public Integer getAno() {
-    return ano;
-  }
-
-  public void setAno(Integer ano) {
-    this.ano = ano;
-  }
-
-  public Usuario getUsuario() {
-    return usuario;
-  }
-
-  public void setUsuario(Usuario usuario) {
-    this.usuario = usuario;
-  }
-
-  public BigDecimal getValor() {
-    return valor;
-  }
-
-  public void setValor(BigDecimal valor) {
-    this.valor = valor;
-  }
-
-  public LocalDate getDataCadastro() {
-    return dataCadastro;
-  }
-
-  public void setDataCadastro(LocalDate dataCadastro) {
-    this.dataCadastro = dataCadastro;
-  }
-
-  public TipoLancamento getTipoLancamento() {
-    return tipoLancamento;
-  }
-
-  public void setTipoLancamento(TipoLancamento tipoLancamento) {
-    this.tipoLancamento = tipoLancamento;
-  }
-
-  public StatusLancamento getStatusLancamento() {
-    return statusLancamento;
-  }
-
-  public void setStatusLancamento(StatusLancamento statusLancamento) {
-    this.statusLancamento = statusLancamento;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
     Lancamento that = (Lancamento) o;
-    return Objects.equals(getId(), that.getId()) && Objects.equals(getMes(), that.getMes()) && Objects.equals(getAno(), that.getAno()) && Objects.equals(getUsuario(), that.getUsuario()) && Objects.equals(getValor(), that.getValor()) && Objects.equals(getDataCadastro(), that.getDataCadastro()) && getTipoLancamento() == that.getTipoLancamento() && getStatusLancamento() == that.getStatusLancamento();
+    return id != null && Objects.equals(id, that.id);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getId(), getMes(), getAno(), getUsuario(), getValor(), getDataCadastro(), getTipoLancamento(), getStatusLancamento());
-  }
-
-  @Override
-  public String toString() {
-    return "Lancamento{" +
-      "id=" + id +
-      ", mes=" + mes +
-      ", ano=" + ano +
-      ", usuario=" + usuario +
-      ", valor=" + valor +
-      ", dataCadastro=" + dataCadastro +
-      ", tipoLancamento=" + tipoLancamento +
-      ", statusLancamento=" + statusLancamento +
-      '}';
+    return getClass().hashCode();
   }
 }
--- src/main/java/com/junio/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/junio/minhasfinancas/model/entity/Usuario.java
@@ -1,10 +1,18 @@
 package com.junio.minhasfinancas.model.entity;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "usuario", schema = "financas")
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
 public class Usuario {
 
   @Id
@@ -21,67 +29,16 @@ public class Usuario {
   @Column(name = "senha")
   private String senha;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getSenha() {
-    return senha;
-  }
-
-  public void setSenha(String senha) {
-    this.senha = senha;
-  }
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
     Usuario usuario = (Usuario) o;
-    return Objects
-      .equals(id, usuario.id) && Objects.equals(nome, usuario.nome) && Objects.equals(email, usuario.email) && Objects.equals(senha, usuario.senha);
+    return id != null && Objects.equals(id, usuario.id);
   }
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-
-    result = prime * result + ((email == null) ? 0 : email.hashCode());
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-    result = prime * result + ((senha == null) ? 0 : senha.hashCode());
-
-    return result;
-  }
-
-  @Override
-  public String toString() {
-    return "Usuario{" +
-      "id=" + id +
-      ", nome='" + nome + '\'' +
-      ", email='" + email + '\'' +
-      ", senha='" + senha + '\'' +
-      '}';
+    return getClass().hashCode();
   }
 }
