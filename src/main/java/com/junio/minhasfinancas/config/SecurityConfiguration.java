b030ab15dfebacb18aa2b3e3cff04a061104e749
libiojunio <libiojss@gmail.com>
2022-08-22 21:43:32 -0300

    Criando senha temporaria.

--- src/main/java/com/junio/minhasfinancas/config/SecurityConfiguration.java
+++ src/main/java/com/junio/minhasfinancas/config/SecurityConfiguration.java
@@ -1,16 +1,25 @@
 package com.junio.minhasfinancas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Bean
+  public PasswordEncoder passwordEncoder () {
+    return new BCryptPasswordEncoder();
+  }
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    super.configure(auth);
+    String senhaCod = passwordEncoder().encode("qwe123");
+    auth.inMemoryAuthentication().withUser("minhas-financas").password(senhaCod).roles("USER");
   }
 
   @Override
